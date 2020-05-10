00d4acb672981bd339d590ff86c604d2868c5b40
mickoangelolacap <mickoangelolacap@gmail.com>
2020-05-10 23:06:26 +0800

    re-edit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+document.querySelector('#consignment').addEventListener('click', () => {
+	document.querySelector('#image').src="assets/images/consignment.jpg"
+})
+
+document.querySelector('#custom').addEventListener('click', () => {
+	document.querySelector('#image').src="assets/images/custom.jpg"
+})
+
+document.querySelector('#trucking').addEventListener('click', () => {
+	document.querySelector('#image').src="assets/images/trucking.jpg"
+})
+
+document.querySelector('#consultation').addEventListener('click', () => {
+	document.querySelector('#image').src="assets/images/consultation.jpg"
+})
+
+document.querySelector('#trading').addEventListener('click', () => {
+	document.querySelector('#image').src="assets/images/trading.jpg"
+})
